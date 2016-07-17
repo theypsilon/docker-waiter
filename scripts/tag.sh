de5928dc0e7695d69125c302462812843eb37a87
José manuel Barroso Galindo <theypsilon@gmail.com>
2016-07-17 17:54:48 +0700

    changing tags

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -5,12 +5,8 @@ set -euo pipefail
 source scripts/common.source
 
 # tagging
-rm ${TAGGED_IMAGES_FILE} || true
 for service in ${SERVICES}; do
 	IMAGE_LATEST="${IMAGE_TEMPLATE/SERVICE/${service}}:latest"
 	IMAGE_TAGGED="${IMAGE_TEMPLATE/SERVICE/${service}}:${VERSION}"
 	docker tag ${IMAGE_LATEST} ${IMAGE_TAGGED}
-	echo ${IMAGE_LATEST} >> ${TAGGED_IMAGES_FILE}
-	echo ${IMAGE_TAGGED} >> ${TAGGED_IMAGES_FILE}
 done
-
