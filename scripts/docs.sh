96a00e6190793dfb9dcf42b3982e500c484bc4a4
José manuel Barroso Galindo <theypsilon@gmail.com>
2016-07-17 19:10:18 +0700

    fixing docs

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -7,7 +7,7 @@ source scripts/common.source
 cp scripts/README.header.md README.latest.md
 for service in ${SERVICES}; do
 	echo >> README.latest.md
-	echo "### ${IMAGE_TEMPLATE/SERVICE/${service}}:${BRANCH}" >> README.latest.md
+	echo "### ${IMAGE_TEMPLATE/SERVICE/${service}}:latest" >> README.latest.md
 	echo >> README.latest.md
 	echo "Configuration of ${service} would be done with following vars:">> README.latest.md
 	echo >> README.latest.md
