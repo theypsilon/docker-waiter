42f69952d1a64bd9a6e7d524dad6f44336703030
José manuel Barroso Galindo <theypsilon@gmail.com>
2016-07-17 16:09:45 +0700

    release changes

--- release.sh
+++ release.sh
@@ -3,6 +3,14 @@
 set -euo pipefail
 
 readonly CANDIDATE_VERSION=${1}
+readonly BACKUP_COMMIT=$(git rev-parse HEAD)
+
+readonly MODIFIED_FILES=$(git ls-files -m)
+if [[ "${MODIFIED_FILES}" != "" ]] ; then
+	echo "ERROR: Following files are in a modified status, commit them or stash them."
+	echo "${MODIFIED_FILES}"
+	exit 1
+fi
 
 ./build.sh
 
@@ -12,23 +20,33 @@ sed -i -e "s/latest/${CANDIDATE_VERSION}/g" README.md
 git add README.md
 git commit -m "New version ${CANDIDATE_VERSION}" 
 git tag ${CANDIDATE_VERSION}
-git checkout ${CANDIDATE_VERSION}
 
-source scripts/common.source
+(
+	git checkout ${CANDIDATE_VERSION}
 
-if [[ ${VERSION} != ${CANDIDATE_VERSION} ]] ; then
-	echo "ERROR: VERSION does not match CANDIDATE_VERSION."
-fi
+	source scripts/common.source
 
-if [[ ${VERSION} == "latest" ]] ; then
-	echo "ERROR: can't release latest, create a tag before running this command."
-fi
+	if [[ ${VERSION} != ${CANDIDATE_VERSION} ]] ; then
+		echo "ERROR: VERSION does not match CANDIDATE_VERSION."
+	fi
 
-./scripts/tag.sh
+	if [[ ${VERSION} == "latest" ]] ; then
+		echo "ERROR: can't release latest, create a tag before running this command."
+	fi
 
-while read image; do
-	docker push ${image}
-done < ${TAGGED_IMAGES_FILE}
+	./scripts/tag.sh
 
-git push origin ${VERSION}
+	while read image; do
+		docker push ${image}
+	done < ${TAGGED_IMAGES_FILE}
+
+	git push origin ${VERSION}
+
+) || (
+	git tag -d ${CANDIDATE_VERSION}
+	git checkout latest
+	git reset HEAD~1
+)
+
 git checkout latest
+git push origin latest
