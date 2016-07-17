b6ddea4b651b96892d6cdd85f20acb73a32dcf71
José manuel Barroso Galindo <theypsilon@gmail.com>
2016-07-17 17:22:44 +0700

    cleanup

--- release.sh
+++ release.sh
@@ -10,38 +10,9 @@ if [[ "${MODIFIED_FILES}" != "" ]] ; then
 	exit 1
 fi
 
+./scripts/new_version.sh
 
-readonly LATEST_TAG=$(git describe --tag)
-if ! [[ ${LATEST_TAG} =~ ([0-9]*).([0-9]*).([0-9]*) ]] ; then
-	echo "ERROR: last tag is wrong formatted"
-fi
-
-MAJOR=${BASH_REMATCH[1]}
-MINOR=${BASH_REMATCH[2]}
-PATCH=${BASH_REMATCH[3]}
-
-case "${1:---patch}" in
-	--minor)
-		echo -n "Realeasing MINOR: "
-	    MINOR=$(($MINOR + 1))
-	    ;;
-	--major)
-		echo -n "Realeasing MAJOR: "
-	    MAJOR=$(($MAJOR + 1))
-	    ;;
-	--patch)
-		echo -n "Realeasing PATCH: " 
-	    PATCH=$(($PATCH + 1))
-	    ;;
-	*)
-		echo "ERROR: Unrecognized option '${1}'"
-		echo "Usage: ${0} [--major] | [--minor] | [--patch]"
-		exit 1
-esac
-readonly CANDIDATE_VERSION="${MAJOR}.${MINOR}.${PATCH}"
-
-echo "VERSION: ${CANDIDATE_VERSION}"
-
+readonly CANDIDATE_VERSION=$(cat candidate-version.txt)
 readonly BACKUP_COMMIT=$(git rev-parse HEAD)
 
 ./build.sh
@@ -53,33 +24,38 @@ git add README.md
 git commit -m "New version ${CANDIDATE_VERSION}" 
 git tag ${CANDIDATE_VERSION}
 
-(
-	git checkout ${CANDIDATE_VERSION}
-
-	source scripts/common.source
-
-	if [[ ${VERSION} != ${CANDIDATE_VERSION} ]] ; then
-		echo "ERROR: VERSION does not match CANDIDATE_VERSION."
-	fi
-
-	if [[ ${VERSION} == "latest" ]] ; then
-		echo "ERROR: can't release latest, create a tag before running this command."
-	fi
-
-	./scripts/tag.sh
-
-	$asdfsd-asdfasd
-
-	while read image; do
-		docker push ${image}
-	done < ${TAGGED_IMAGES_FILE}
-
-	git push origin ${VERSION}
-	git checkout latest
-	git push origin latest
-
-) || (
+error_handling() {
 	git tag -d ${CANDIDATE_VERSION}
 	git checkout latest
 	git reset HEAD ${BACKUP_COMMIT}
-)
+}
+
+trap error_handling EXIT
+
+git checkout ${CANDIDATE_VERSION}
+
+source scripts/common.source
+
+if [[ ${VERSION} != ${CANDIDATE_VERSION} ]] ; then
+	echo "ERROR: VERSION does not match CANDIDATE_VERSION."
+	exit 1
+fi
+
+if [[ ${VERSION} == "latest" ]] ; then
+	echo "ERROR: can't release latest, create a tag before running this command."
+	exit 1
+fi
+
+./scripts/tag.sh
+
+$asdfsd-asdfasd
+
+while read image; do
+	docker push ${image}
+done < ${TAGGED_IMAGES_FILE}
+
+git push origin ${VERSION}
+git checkout latest
+git push origin latest
+
+trap - EXIT
